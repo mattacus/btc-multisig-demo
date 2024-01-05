c3e8021f73b3ffd548f44d877a2a7de7b9e4fdb4
mattl <matt.lee.personal@gmail.com>
2024-01-04 21:27:39 -0600

    add p2sh address endpoint

--- server/api.py
+++ server/api.py
@@ -1,8 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from services import (
     get_latest_block_tip_txn,
     get_testnet_funding_addresses,
     get_address_basic_info,
+    create_address_p2sh,
 )
 from blockstream_api import blockstream
 
@@ -22,3 +23,11 @@ def funding_addresses():
 @api.route("address/<address>/info")
 def address_info(address):
     return get_address_basic_info(address)
+
+
+@api.route("address/get_p2sh_multisig_address", methods=["POST"])
+def get_p2sh_multisig_address():
+    data = request.get_json()
+    pubkeys = data.get("pubkeyList")
+    quorum = data.get("quorum")
+    return create_address_p2sh(pubkeys, quorum)
--- server/services.py
+++ server/services.py
@@ -1,5 +1,7 @@
 import logging
+import json
 from buidl import TxFetcher, PrivateKey
+from buidl.script import RedeemScript
 from blockstream_api import blockstream
 from util import format_plaintext_private_key_secret, sat_to_btc
 from settings import TESTNET_SECRET_REACH, TESTNET_SECRET_HARVEST, BITCOIN_NETWORK
@@ -50,3 +52,22 @@ def get_address_basic_info(address):
             "funded_utxo_sum": sat_to_btc(addr_info["chain_stats"]["funded_txo_sum"]),
         }
     return {}
+
+
+def create_address_p2sh(pubkeys, quorum):
+    """
+    Given a list of public keys, create a P2SH address and return it as well as the redeem script
+    """
+    if len(pubkeys) < 2:
+        raise ValueError("Must provide at least two public keys")
+
+    redeem_script = RedeemScript.create_p2sh_multisig(
+        quorum_m=quorum,
+        pubkey_hexes=pubkeys,
+        sort_keys=False,
+    )
+
+    return {
+        "address": redeem_script.address(BITCOIN_NETWORK),
+        "redeem_script": redeem_script.__repr__(),
+    }
